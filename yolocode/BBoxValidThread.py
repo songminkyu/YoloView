492723407cd9cf00ce29944f3dedb4dd28fb0a0e
song minkyu <mksong@finaldata.com>
2024-11-22 09:32:58 +0900

    BBoxValidThread.py add

---
+++ yolocode/BBoxValidThread.py
@@ -0,0 +1,7 @@
+from yolocode.YOLOv8Thread import YOLOv8Thread
+from ultralytics.engine.results import Results
+from ultralytics.utils import ops
+
+class BBoxValidThread(YOLOv8Thread):
+    def __init__(self):
+        super(YOLOv8Thread, self).__init__()
