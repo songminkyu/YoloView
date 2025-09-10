f6b2cf87f1a778b106011f0ca8e7bd03d1c99da7
songminkyu <rabbircarrot@naver.com>
2025-09-10 16:03:11 +0900

    add : add pp-structureV3.py sample

---
+++ examples/pp-structureV3.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from paddleocr import PPStructureV3
+
+input_file = "./pc-spec-markdown-table.pdf"
+output_path = Path("./output")
+
+pipeline = PPStructureV3(lang="korean")
+output = pipeline.predict(input=input_file)
+
+markdown_list = []
+markdown_images = []
+
+for res in output:
+    md_info = res.markdown
+    markdown_list.append(md_info)
+    markdown_images.append(md_info.get("markdown_images", {}))
+
+markdown_texts = pipeline.concatenate_markdown_pages(markdown_list)
+
+mkd_file_path = output_path / f"{Path(input_file).stem}.md"
+mkd_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+with open(mkd_file_path, "w", encoding="utf-8") as f:
+    f.write(markdown_texts)
+
+for item in markdown_images:
+    if item:
+        for path, image in item.items():
+            file_path = output_path / path
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            image.save(file_path)
